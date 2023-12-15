3823b8ab7cab0d0b251d93903feb9e9329a5eb63
HiYo0 <151518028+HiYo0@users.noreply.github.com>
2023-12-15 18:06:45 +0900

    과제 123123

--- day13/step2.js
+++ day13/step2.js
@@ -101,3 +101,83 @@ function 출차기록(){
     // alert("출차완료")
 
 } // function끝
+
+
+// ==============================================
+const 등록 = [];
+const 번호 = [];
+const 시간s = [];
+const 시간e = [];
+const 사용자 = [];
+
+const 사용시간 = [];
+const 총금액 = 0; /* 하루매출 */
+
+function 등록함수(){        // f s
+    console.log('등록함수()실행')
+
+    // 입력
+    const 번호2 =  document.querySelector('#pcNum').value; console.log(번호2)
+    const 시작시간 = document.querySelector('#시작시간1').value; console.log(시작시간)
+    const 종료시간 = document.querySelector('#종료시간1').value; console.log(종료시간)
+    const 사용자2 = document.querySelector('#사용자1').value; console.log(사용자2)
+    const 사용시간2 = document.querySelector('#사용시간1').value; console.log(사용시간2)
+   
+    // 처리
+    번호.push( 번호2 );         console.log(번호)
+    시간s.push( 시작시간 );     console.log(시간s);
+    시간e.push( 종료시간 );     console.log(시간e);
+    사용자.push( 사용자2 );     console.log(사용자);
+    사용시간.push( 사용시간2 );    console.log(사용시간);
+   
+    // 출력
+    출력함수();
+    
+} // f e
+
+
+function 출력함수(){
+    
+    let 출력함수 = '';
+
+    for(let i = 0 ; i < 사용자.length ; i++ ){ 
+        출력함수 +=`
+        <div class="content1">
+        <h1>${번호[i]}</h1>
+        </div>
+
+        <div class="content1">
+            <h1>${시간s[i]}</h1>
+        </div>
+
+        <div class="content1">
+            <h1>${시간e[i]}</h1>
+        </div>
+
+        <div class="content1">
+            <h1>${사용자[i]}</h1>
+        </div>
+
+        <div class="content1">
+            <h1>${사용시간[i]}</h1>
+        </div>
+
+        <div class="content1">
+            <h1><input id="삭제함수버튼" onclick="삭제함수( ${ i } )" type="button" value="삭제" /></h1>
+        </div>
+        `
+    }
+    document.querySelector('#print').innerHTML = 출력함수
+    
+}
+
+function 삭제함수(삭제할인덱스){         // f s
+    
+번호.splice(삭제할인덱스,1);
+시간s.splice(삭제할인덱스,1);
+시간e.splice(삭제할인덱스,1);
+사용자.splice(삭제할인덱스,1);
+사용시간.splice(삭제할인덱스,1);
+
+출력함수();
+} // f e 
---
+++ day16/step1.js
@@ -0,0 +1,16 @@
+function A(B){
+    console.log("1")
+    console.log("1")
+    console.log("1")
+    console.log("1진행")
+    B= 4;
+    // return console.log(B+2)
+}
+Q= 123000;
+console.log(typeof(Q));
+
+B=String(Q);
+B.split(',');
+console.log(B);
+console.log(typeof(B));
+
