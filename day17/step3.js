6c4dc190525c52c2acf3d1537aa71b8e75860d36
HiYo0 <151518028+HiYo0@users.noreply.github.com>
2023-12-18 18:04:59 +0900

    123123

--- day17/step3.js
+++ day17/step3.js
@@ -12,7 +12,7 @@
 
             const boardArray  = [ ]
 
-            // 1. [C]글쓰기 [ 글쓰기 버튼을 클릭했을때. ]
+            // 1. [C]글쓰기 [ 글쓰기 버튼을 클릭 했을때. ]
             function 글쓰기(){  // 매개변수 : X // 리턴값 : X
                 // 1. HTML로 부터 입력받은 값 가져오기 
                 const writer = document.querySelector('#writer').value;
@@ -21,11 +21,17 @@
                 const content = document.querySelector('#content').value;
                 /* 입력값 받아오기 */
 
-                // 2. 배열에 저장하기
-                const board = `${writer}/${passowrd}/${title}/${content}`;  console.log( board );
+                // 2. 객체에 저장하기
+                const board = {
+                    작성자 : writer , 
+                    비밀번호 : passowrd , 
+                    제목 : title , 
+                    내용 : content
+                }
+                board.조회수 = 0;   // 객체내 없는 key 호출시 key생성됨.    // 이제 등록했으니까 조회수는 0부터 시작함
                 /* /넣어서 지정한 이유 == 배열내용을 구분지을라고  */
 
-                boardArray.push( board );   console.log( boardArray );
+                boardArray.push( board );
                 // 3. 배열에 HTML에 출력하기
 
                 모든글출력();
@@ -34,42 +40,34 @@
                 document.querySelector('#passowrd').value = "";
                 document.querySelector('#title').value = "";
                 document.querySelector('#content').value = "";
-                /* input에입력되어있는 값을 지움(공백) */
 
                 return;
             }
-            // 2-1. [R]모든글출력 [ 1.js가 열렸을때 2.글쓰기/글수정/글삭제 처리 할때마다 ]
-            function 모든글출력(){ // 매개변수 : X // 리턴값 : X
+            
+            function 모든글출력(){
                 const tableBody = document.querySelector('table>tbody');
-                /* tableBody의 출력을 위한 좌표 지정 */
                 let html = ``;
                 for( let i = 0 ; i<boardArray.length ; i++ ){
-                    console.log(boardArray.length);
-                    console.log( boardArray[i] );
-                    // 저장할떄 4가지 속성/상태 값들을 "/" 기준으로 하나로 묶었으니까. 다시 "/" 기준으로 분리[ split() ] 하자.
-                    console.log( boardArray[i].split("/") );
                     html += `<tr onclick="개별글출력( ${i} )" >
                                 <th>${ i }</th>   
-                                <th>${ boardArray[i].split("/")[2] }</th>  
-                                <th>${ boardArray[i].split("/")[0] }</th>
+                                <th>${ boardArray[i].제목 }</th>  
+                                <th>${ boardArray[i].작성자 }</th>
+                                <th>${ boardArray[i].조회수 }</th>
                             </tr>`
                 }
                 tableBody.innerHTML = html;
                 return;
             }
-            // 2-2 [R]개별글출력  [ 해당 글을 선택했을때. ]
-            function 개별글출력( index ){ // 매개변수 : (대상)보고자하는 식별번호 // 리턴값 : X
-                /* 모든글출력을 했을때 등장하는 tr영역을 클릭했을때 실행되는 함수이름입니다. 배열의 몇번째 글에 해당하는 함수인지 식별하기 위한 매개변수입니다. */
-                console.log(index);
+            
+            function 개별글출력( index ){
+
+                // *조회수 증가.
+                boardArray[index].조회수 += 1;모든글출력()
                 const boardBox = document.querySelector('#boardBox');
-                /* boardBox 상수는 HTML에 출력하기위한 좌표를 정의 해둔것 */
-                // - replaceAll ( '교체할문자열" , "새로운문자열" )
-                // - \n을 <br/> 변경 : <textarea> 에서 줄바꿈처리하면 \n으로  저장되고 HTML 출력할때 \n 대신 <br/> 치환
-                console.log(boardArray[index].split("/")[3].replaceAll('\n','<br/>'));
                 
-                let html = `<div> 작성자 : ${boardArray[index].split("/")[0]} </div>
-                            <div> 제목 : ${boardArray[index].split("/")[2]} </div>
-                            <div> 내용 : ${boardArray[index].split("/")[3].replaceAll('\n','<br/>')} </div>
+                let html = `<div> 작성자 : ${boardArray[index].작성자} 조회수 : ${boardArray[index].조회수}</div>
+                            <div> 제목 : ${boardArray[index].제목} </div>
+                            <div> 내용 : ${boardArray[index].내용.replaceAll('\n','<br/>')} </div>
                             <input onclick="(글수정${index})" type="button" value="수정"/>
                             <input onclick="글삭제(${index})" type="button" value="삭제"/>
                             `;
@@ -89,13 +87,14 @@
             
                 // 1. 검증[유효성검사] - 패스워드를 입력받아.. 해당 게시물의 패스워드와 동일하면.
                 const checkPw = prompt('비밀번호');
-                if(checkPw == boardArray[index].split("/")[1]){ // 만약에 입력받은 패스워드와 게시물의 패스워드가 일치하면
+                if(checkPw == boardArray[index].비밀번호){ // 만약에 입력받은 패스워드와 게시물의 패스워드가 일치하면
                     // 수정할 값들을 입력받아.
                     const updateTitle = prompt('수정할 제목');
                     const updateContent = prompt('수정할 내용');
                     // 수정처리
-                    console.log(boardArray[index]);
-                    boardArray[index] = `${boardArray[index].split('/')[0]}/${boardArray[index].split('/')[1]}/${updateTitle}/${updateContent}`
+                    boardArray[index].제목 = updateTitle;
+                    boardArray[index].내용 = updateContent;
+
                     alert('수정성공');
                     
                     // 새로고침
@@ -109,9 +108,8 @@
             }
             // 4. [D]글삭제 [ 글삭제 버튼을 클릭했을때.]
             function 글삭제( index ){ // 매개변수 : (대상)삭제할 식별번호  // 리턴값 : X
-                console.log(index);
                 const checkPw = prompt('비밀번호')
-                if( checkPw == boardArray[index].split("/")[1]){
+                if( checkPw == boardArray[index].비밀번호 ){
                     // 삭제처리
                     boardArray.splice( index, 1);
                     alert('삭제 성공');
@@ -121,7 +119,6 @@
                 }else{
                     alert('패스워드가 가릅니다. 삭제 불가.')
                 }
-                
 
                 return;
             }
